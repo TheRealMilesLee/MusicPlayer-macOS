070c0e0ede2b68fe3529f74f5276bab1fe548ee1
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-09-29 22:05:38 -0500

    Update the local playlists table view

--- MusicPlayer/MusicPlayer/LocalPlaylist.swift
+++ MusicPlayer/MusicPlayer/LocalPlaylist.swift
@@ -7,11 +7,48 @@
 
 import SwiftUI
 
+struct Playlists: Identifiable
+{
+  let id: Int
+  let Title: String
+  let Duration: String
+  let Artist: String
+  let Album: String
+}
 struct LocalPlaylist: View
 {
+  @State private var playlists = [
+    Playlists(id: 1, Title: "Creep", Duration: "3:59", Artist: "Radiohead", Album: "Pablo Honey"),
+    Playlists(id: 2, Title: "Round and Round", Duration: "3:18", Artist: "Imagine Dragons", Album: "Night Visions")
+  ]
   var body: some View
   {
     Text("Playlists").padding(.all).font(.headline)
+    Table(playlists)
+    {
+      TableColumn("Title", value: \.Title)
+      TableColumn("Duration", value: \.Duration)
+      TableColumn("Artist", value: \.Artist)
+      TableColumn("Album", value: \.Album)
+    }
+  }
+}
+
+func showOpenPanel() -> URL?
+{
+  let openPanel = NSOpenPanel()
+  openPanel.allowsMultipleSelection = true
+  openPanel.canChooseDirectories = true
+  openPanel.canChooseFiles = true
+  let response = openPanel.runModal()
+
+  if (response == .OK)
+  {
+    return openPanel.url
+  }
+  else
+  {
+    return nil
   }
 }
 
