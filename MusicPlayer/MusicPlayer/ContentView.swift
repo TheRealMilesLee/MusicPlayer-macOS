c6ee98ab55b173e413d65da89aaab8a3b49ca241
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-09-30 22:12:29 -0500

    Update the local playlists table view

--- MusicPlayer/MusicPlayer/ContentView.swift
+++ MusicPlayer/MusicPlayer/ContentView.swift
@@ -37,69 +37,67 @@ struct ContentView: View
             NavigationLink(destination: Songs()){Label("Songs", systemImage: "music.note")}
             NavigationLink(destination: Connect()){Label("Connect", systemImage: "music.note.tv")}
           }.padding(.bottom)
-
-
         }
-      }
-    } .navigationTitle("Welcome")
-    Divider()
-    HStack
-    {
-      Button(action: {})
-      {
-        HStack
-        {
-          Image("Cover").resizable().frame(width: 25, height: 25).shadow(radius: 6, x: 0, y: 3).padding(.all)
-          Text("Creep").padding(.leading, 10)
-          Slider(value: $SliderInitialplace,in: 0...100)
-        }
-        
-      }.buttonStyle(PlainButtonStyle())
-      Spacer()
+      }.moveDisabled(false)
+      Divider()
       HStack
       {
         Button(action: {})
         {
-          Image(systemName: "backward.fill").font(.title3)
-        }.buttonStyle(PlainButtonStyle()).padding(.leading, 30)
-        
-        Button(action:{
-          isPlaying.toggle()
-          do
+          HStack
           {
-            let url = Bundle.main.path(forResource: "Blue", ofType: "mp3")
-            audioPlayer =  try AVAudioPlayer(contentsOf: URL(fileURLWithPath: url!))
-            let player = audioPlayer
-            if (isPlaying)
+            Image("Cover").resizable().frame(width: 25, height: 25).shadow(radius: 6, x: 0, y: 3).padding(.all)
+            Text("Creep").padding(.leading, 10)
+            Slider(value: $SliderInitialplace,in: 0...100)
+          }
+          
+        }.buttonStyle(PlainButtonStyle())
+        Spacer()
+        HStack
+        {
+          Button(action: {})
+          {
+            Image(systemName: "backward.fill").font(.title3)
+          }.buttonStyle(PlainButtonStyle()).padding(.leading, 30)
+          
+          Button(action:{
+            isPlaying.toggle()
+            do
             {
-              player?.prepareToPlay()
-              player?.play()
+              let url = Bundle.main.path(forResource: "Blue", ofType: "mp3")
+              audioPlayer =  try AVAudioPlayer(contentsOf: URL(fileURLWithPath: url!))
+              let player = audioPlayer
+              if (isPlaying)
+              {
+                player?.prepareToPlay()
+                player?.play()
+              }
+              else
+              {
+                player?.pause()
+              }
+            } catch
+            {
+              print("Error info: \(error)")
+            }
+          })
+          {
+            if (!isPlaying)
+            {
+              Image(systemName: "play.fill").font(.title3)
             }
             else
             {
-              player?.pause()
+              Image(systemName: "pause.fill").font(.title3)
             }
-          } catch
+          }.buttonStyle(PlainButtonStyle()).padding(.horizontal)
+          
+          Button(action: {})
           {
-            print("Error info: \(error)")
-          }
-        })
-        {
-          if (!isPlaying)
-          {
-            Image(systemName: "play.fill").font(.title3)
-          }
-          else
-          {
-            Image(systemName: "pause.fill").font(.title3)
-          }
-        }.buttonStyle(PlainButtonStyle()).padding(.horizontal)
-        
-        Button(action: {})
-        {
-          Image(systemName: "forward.fill").font(.title3)
-        }.buttonStyle(PlainButtonStyle()).padding(.trailing, 30)
-      }.buttonStyle(PlainButtonStyle())
+            Image(systemName: "forward.fill").font(.title3)
+          }.buttonStyle(PlainButtonStyle()).padding(.trailing, 30)
+        }.buttonStyle(PlainButtonStyle())
+      }
     }
   }
 }
