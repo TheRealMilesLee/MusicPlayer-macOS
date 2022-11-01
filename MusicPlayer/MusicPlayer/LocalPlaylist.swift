60ddcdc786d1dd06ea733dcd3584427ee6a5c9d1
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-10-31 20:08:23 -0500

    Problem on the LocalPlaylist

--- MusicPlayer/MusicPlayer/LocalPlaylist.swift
+++ MusicPlayer/MusicPlayer/LocalPlaylist.swift
@@ -1,12 +1,5 @@
-  //
-  //  LocalPlaylist.swift
-  //  MusicPlayer
-  //
-  //  Created by Silverhand on 2022/9/27.
-  //
 
 import SwiftUI
-
 struct Playlists: Identifiable
 {
   let id: Int
@@ -23,53 +16,53 @@ struct LocalPlaylist: View
     Text("Playlists").padding(.all).font(.headline)
     Table()
     {
-      TableColumn("Title", value: "Test tITLE")
-      TableColumn("Duration", value: "tEST Duration")
-      TableColumn("Artist", value: "Test Artist")
-      TableColumn("Album", value: "Test Album")
+      TableColumn("Title", value: "A Song")
+      TableColumn("Duration", value: "3:59")
+      TableColumn("Artist", value: "Michael Jackson")
+      TableColumn("Album", value: "Yee-ha")
     }
   }
 }
-
-func readFile() -> [Playlists]
-{
-  var formatted_playlists: [Playlists] = []
-  let FileHandler_user = FileManager.default
-  let user_folder = showOpenPanel()
-  do
-  {
-    let contents = try FileHandler_user.contentsOfDirectory(atPath: user_folder!.path)
-    print(contents)
-    var index_iter = 0
-    for content in contents
-    {
-      formatted_playlists.append(Playlists(id:index_iter, Title: content, Duration: "3:59", Artist: "testArtist", Album: "TestAlbum"))
-      index_iter += 1
-    }
-  }
-  catch
-  {
-    print("Problem on reading folder")
-  }
-  return formatted_playlists
-}
-
-func showOpenPanel() -> URL?
-{
-  let openPanel = NSOpenPanel()
-  openPanel.allowsMultipleSelection = true
-  openPanel.canChooseDirectories = true
-  openPanel.canChooseFiles = true
-  let response = openPanel.runModal()
-  let FileHandler_user = FileManager.default
-  let FileURL = FileHandler_user.urls(for: .musicDirectory, in: .userDomainMask)
-  let defaultURL = FileURL[0] as URL
-  if (response == .OK)
-  {
-    return openPanel.url
-  }
-  else
-  {
-    return defaultURL
-  }
-}
+//
+//func readFile() -> [Playlists]
+//{
+//  var formatted_playlists: [Playlists] = []
+//  let FileHandler_user = FileManager.default
+//  let user_folder = showOpenPanel()
+//  do
+//  {
+//    let contents = try FileHandler_user.contentsOfDirectory(atPath: user_folder!.path)
+//    print(contents)
+//    var index_iter = 0
+//    for content in contents
+//    {
+//      formatted_playlists.append(Playlists(id:index_iter, Title: content, Duration: "3:59", Artist: "testArtist", Album: "TestAlbum"))
+//      index_iter += 1
+//    }
+//  }
+//  catch
+//  {
+//    print("Problem on reading folder")
+//  }
+//  return formatted_playlists
+//}
+//
+//func showOpenPanel() -> URL?
+//{
+//  let openPanel = NSOpenPanel()
+//  openPanel.allowsMultipleSelection = true
+//  openPanel.canChooseDirectories = true
+//  openPanel.canChooseFiles = true
+//  let response = openPanel.runModal()
+//  let FileHandler_user = FileManager.default
+//  let FileURL = FileHandler_user.urls(for: .musicDirectory, in: .userDomainMask)
+//  let defaultURL = FileURL[0] as URL
+//  if (response == .OK)
+//  {
+//    return openPanel.url
+//  }
+//  else
+//  {
+//    return defaultURL
+//  }
+//}
