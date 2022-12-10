33f8f3b730a63c6af480f42d60209af1ccee7f1c
TheRealMilesLee <grandtheftauto710@outlook.com>
2022-12-10 00:01:09 -0600

    update the file read in

--- MusicPlayer/MusicPlayer/LocalPlaylist.swift
+++ MusicPlayer/MusicPlayer/LocalPlaylist.swift
@@ -6,32 +6,46 @@ import AVFoundation
 struct LocalPlaylist: View
 {
   @State var load_file : Bool = false
-  @State private var selectedSongs = Set<Playlists.ID>()
+  @State private var selectedSongs: Playlists.ID?
   @State private var AccessFile: Array<Playlists> = Array()
+  @State private var sortOrder = [KeyPathComparator(\Playlists.Title)]
   var body: some View
   {
-    VStack
-    {
-      Text("Playlists").padding(.all).font(.headline)
-      Button("Load from File", action:{
-        let contents = getFileNameArray()
-        let PlaylistArray = MusicPlayFileArray()
-        for content in 0..<contents.count
-        {
-            AccessFile.append(Playlists(Title: contents[content], Duration: "3:59",  Artist: "testArtist", Album: "TestAlbum", image: "Bah"))
-        }
-      })
-      Table(AccessFile, selection: $selectedSongs)
+    Text("Playlists").padding(.all).font(.headline)
+    Button("Load from File", action:{
+      let contents = getFileNameArray()
+      for content in 0..<contents.count
       {
-        TableColumn("Title", value: \.Title)
-        TableColumn("Duration", value: \.Duration)
-        TableColumn("Artist", value: \.Artist)
-        TableColumn("Album", value: \.Album)
+        AccessFile.append(Playlists(Title: contents[content], Duration: "3:59",  Artist: "testArtist", Album: "TestAlbum", image: "Bah"))
       }
-      Text("\(selectedSongs.description) is selected")
+    })
+    Table(AccessFile, selection: $selectedSongs, sortOrder: $sortOrder)
+    {
+      TableColumn("Title", value: \.Title)
+      TableColumn("Duration", value: \.Duration)
+      TableColumn("Artist", value: \.Artist)
+      TableColumn("Album", value: \.Album)
     }
+
+  }
+  func FindTitle(AccessFile: [Playlists]) -> String
+  {
+    for NameIndex in AccessFile
+    {
+      if (NameIndex.id == selectedSongs)
+      {
+        return NameIndex.Title
+      }
+    }
+    return "Null"
   }
 }
 
+
+//if (selectedSongs?.description != nil)
+//{
+//  let Result = FindTitle(AccessFile: AccessFile)
+//  Text(Result)
+//}
 
 
