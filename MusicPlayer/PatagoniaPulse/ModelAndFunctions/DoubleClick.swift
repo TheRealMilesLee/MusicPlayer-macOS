7222c07896733fe4a4115a21147ddd02bca97846
TheRealMilesLee <grandtheftauto710@outlook.com>
2023-03-16 14:14:14 -0500

    update

--- MusicPlayer/PatagoniaPulse/ModelAndFunctions/DoubleClick.swift
+++ MusicPlayer/PatagoniaPulse/ModelAndFunctions/DoubleClick.swift
@@ -1,53 +1,55 @@
 import Foundation
 import SwiftUI
 
-extension View {
-    /// Adds a double click handler this view (macOS only)
-    ///
-    /// Example
-    /// ```
-    /// Text("Hello")
-    ///     .onDoubleClick { print("Double click detected") }
-    /// ```
-    /// - Parameters:
-    ///   - handler: Block invoked when a double click is detected
-  func onDoubleClick(handler: @escaping () -> Void) -> some View {
+extension View
+{
+  func onDoubleClick(handler: @escaping () -> Void) -> some View
+  {
     modifier(DoubleClickHandler(handler: handler))
   }
 }
 
-struct DoubleClickHandler: ViewModifier {
+struct DoubleClickHandler: ViewModifier
+{
   let handler: () -> Void
-  func body(content: Content) -> some View {
-    content.overlay {
+  func body(content: Content) -> some View
+  {
+    content.overlay
+    {
       DoubleClickListeningViewRepresentable(handler: handler)
     }
   }
 }
 
-struct DoubleClickListeningViewRepresentable: NSViewRepresentable {
+struct DoubleClickListeningViewRepresentable: NSViewRepresentable
+{
   let handler: () -> Void
-  func makeNSView(context: Context) -> DoubleClickListeningView {
+  func makeNSView(context: Context) -> DoubleClickListeningView
+  {
     DoubleClickListeningView(handler: handler)
   }
   func updateNSView(_ nsView: DoubleClickListeningView, context: Context) {}
 }
 
-class DoubleClickListeningView: NSView {
+class DoubleClickListeningView: NSView
+{
   let handler: () -> Void
-
-  init(handler: @escaping () -> Void) {
+  init(handler: @escaping () -> Void)
+  {
     self.handler = handler
     super.init(frame: .zero)
   }
 
-  required init?(coder: NSCoder) {
+  required init?(coder: NSCoder)
+  {
     fatalError("init(coder:) has not been implemented")
   }
 
-  override func mouseDown(with event: NSEvent) {
+  override func mouseDown(with event: NSEvent)
+  {
     super.mouseDown(with: event)
-    if event.clickCount == 2 {
+    if event.clickCount == 2
+    {
       handler()
     }
   }
