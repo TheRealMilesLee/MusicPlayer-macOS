75c0591412814f75ea9fcc00dd918a12f827dd62
TheRealMilesLee <grandtheftauto710@outlook.com>
2023-11-01 10:44:56 -0700

    Delete unusable code

--- MusicPlayer/PatagoniaPulse/View/ContentView.swift
+++ MusicPlayer/PatagoniaPulse/View/ContentView.swift
@@ -278,45 +278,6 @@ struct ContentView: View
     }
   }
 
-    //  /*---------------------------------------------------- Read in empty handling -------------------------------------------------------------------*/
-    //  /**
-    //   * @brief This function is to handling when the user selected folder does not have any music files
-    //   * @return void
-    //   */
-    //  func selectFolderAndProcess() async
-    //  {
-    //    if (metaDuration.isEmpty)
-    //    {
-    //      let alert = NSAlert()
-    //      alert.messageText = "Error"
-    //      alert.informativeText = "The folder you choose does not have any music."
-    //      alert.alertStyle = .critical
-    //      alert.addButton(withTitle: "Quit")
-    //      let response = alert.runModal()
-    //      if (response == .alertFirstButtonReturn)
-    //      {
-    //        NSApp.terminate(nil)
-    //      }
-    //    }
-    //    for content in 0..<FileNameContents.count
-    //    {
-    //      let BeforechoppedFileName = FileNameContents[content]
-    //      let AfterChoppedFileName = BeforechoppedFileName
-    //        .replacingOccurrences(of: #".mp3"#, with: "")
-    //        .replacingOccurrences(of: #".mp4"#, with: "")
-    //        .replacingOccurrences(of: #".wav"#, with: "")
-    //        .replacingOccurrences(of: #".flac"#, with: "")
-    //        // Check if metaDuration is empty
-    //      let DurationTimeSeconds = CMTimeGetSeconds(metaDuration[content])
-    //      let DurationToMinutes = DurationTimeSeconds / 60
-    //      let DurationRoundMinutes = Double(round(100 * DurationToMinutes) / 100)
-    //      let DurationStringnify = String(DurationRoundMinutes).replacingOccurrences(of: #"."#, with: ":")
-    //      let AlbumImage = NSImage(data: metaArtwork[content] as Data)
-    //      AccessFile.append(Playlists(Title: AfterChoppedFileName, Duration: DurationStringnify,  Artist: metaArtistArray[content], Album: metaAlbumArray[content], image: AlbumImage))
-    //    }
-    //  }
-
-
   /*---------------------------------------------------- Playback Function -------------------------------------------------------------------*/
   /**
    * @brief This function is to control the audio forward
