8820b7a00e503ec8b6061e6403a0a45e446151bc
TheRealMilesLee <grandtheftauto710@outlook.com>
2023-12-03 21:16:48 -0800

    update

--- MusicPlayer/PatagoniaPulse/ModelAndFunctions/DoubleClick.swift
+++ MusicPlayer/PatagoniaPulse/ModelAndFunctions/DoubleClick.swift
@@ -40,7 +40,7 @@ class DoubleClickListeningView: NSView
     super.init(frame: .zero)
   }
 
-  required init?(coder: NSSecureCoding)
+  required init?(coder: NSCoder)
   {
     fatalError("init(coder:) has not been implemented")
   }
