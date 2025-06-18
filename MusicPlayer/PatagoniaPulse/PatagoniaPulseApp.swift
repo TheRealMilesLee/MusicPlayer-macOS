1967ab81e84e6e18cb2c2151155c15199fc6049b
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-06-17 23:36:54 -0700

    Well it still crashed.....i don't know

--- MusicPlayer/PatagoniaPulse/PatagoniaPulseApp.swift
+++ MusicPlayer/PatagoniaPulse/PatagoniaPulseApp.swift
@@ -6,11 +6,12 @@
   //
 
 import SwiftUI
+
 @main
 struct PatagoniaPulseApp: App
 {
   static let MusicVM = MusicViewModel(PatagoniaPulseApp: Playlists.data)
-
+  @State private var reloadID = UUID()
 
   var body: some Scene
   {
@@ -18,7 +19,18 @@ struct PatagoniaPulseApp: App
     {
       ContentView(MusicViewModel: PatagoniaPulseApp.MusicVM)
         .environmentObject(AudioPlayManager())
-        
+//        .id(reloadID)
+    }
+    .commands {
+      CommandMenu("File") {
+        Button("Choose Music Folder…") {
+          if let url = showOpenPanel() {
+            user_folder = url
+            reloadID = UUID()
+          }
+        }
+        .keyboardShortcut("O", modifiers: [.command])
+      }
     }
   }
 }
